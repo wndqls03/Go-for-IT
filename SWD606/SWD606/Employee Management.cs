b108bb7c72948fdd276414f448b08ea2a1c19bbc
Jongeun LEE <ljeljelje9156@gmail.com>
2023-11-30 10:16:40 +1300

    add

--- SWD606/SWD606/Employee Management.cs
+++ SWD606/SWD606/Employee Management.cs
@@ -21,7 +21,60 @@ namespace SWD606
 
         private void add_Btn_Click(object sender, EventArgs e)
         {
+            try
+            {
+                string empID = empID_Text.Text;
+                string empName = empName_Text.Text;
+                int age = Convert.ToInt32(age_Text.Text);
+                string username = userNa_Text.Text;
+                string password = pass_Text.Text;
+                string address = address_Text.Text;
+                string gender = gen_Text.Text;
+                string contactNumber = num_Text.Text;
+                decimal salary = Convert.ToDecimal(sal_Text.Text);
+                string position = posi_List.SelectedItem != null ? posi_List.SelectedItem.ToString() : "";
+                string leaveStatus = status_List.SelectedItem != null ? status_List.SelectedItem.ToString() : "";
 
+                string query = "INSERT INTO Customer (EmployeeID, EmployeeName, Age, Username, Password, Address, Gender, ContactNumber, Salary, Position, LeaveStatus) " +
+                               "VALUES (@EmployeeID, @EmployeeName, @Age, @Username, @Password, @Address, @Gender, @ContactNumber, @Salary, @Position, @LeaveStatus)";
+
+                string connectionString = "Data Source=(localdb)\\Local;Initial Catalog=Customer;Integrated Security=True;Connect Timeout=30";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@EmployeeID", empID);
+                        command.Parameters.AddWithValue("@EmployeeName", empName);
+                        command.Parameters.AddWithValue("@Age", age);
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@Password", password);
+                        command.Parameters.AddWithValue("@Address", address);
+                        command.Parameters.AddWithValue("@Gender", gender);
+                        command.Parameters.AddWithValue("@ContactNumber", contactNumber);
+                        command.Parameters.AddWithValue("@Salary", salary);
+                        command.Parameters.AddWithValue("@Position", position);
+                        command.Parameters.AddWithValue("@LeaveStatus", leaveStatus);
+
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Data added successfully.");
+                            DisplayEmployeeData();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Data insertion failed.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
 
         private void del_Btn_Click(object sender, EventArgs e)
